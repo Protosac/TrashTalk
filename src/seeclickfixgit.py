2dc90da0510a216c9d2949be0d811f910b0e2dcf
TangoYankee <miller.tim108@gmail.com>
2017-01-26 18:15:16 -0800

    Test Case

---
+++ src/seeclickfixgit.py
@@ -0,0 +1,9 @@
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+
+__author__ = "miller.tim"
+__date__ = "$Jan 26, 2017 6:06:40 PM$"
+
+if __name__ == "__main__":
+    print "Hello World"
